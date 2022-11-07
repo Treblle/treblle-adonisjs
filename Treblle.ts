d61f876794d0bdf5769b3ef13f6dea797a91ce9a
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2022-11-07 13:54:09 +0100

    feat: move utils to package

--- Treblle.ts
+++ Treblle.ts
@@ -2,69 +2,9 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import type { ConfigContract } from '@ioc:Adonis/Core/Config'
 import { inject } from '@adonisjs/core/build/standalone'
 import os from 'os'
+import { generateFieldsToMask, maskSensitiveValues, getRequestDuration } from '@treblle/utils'
 import fetch from 'node-fetch'
 
-function getRequestDuration(startTime): number {
-  const NS_PER_SEC = 1e9
-  const NS_TO_MICRO = 1e3
-  const diff = process.hrtime(startTime)
-
-  const microseconds = (diff[0] * NS_PER_SEC + diff[1]) / NS_TO_MICRO
-
-  return Math.ceil(microseconds)
-}
-
-function generateFieldsToMask(additionalFieldsToMask = []) {
-  const defaultFieldsToMask = [
-    'password',
-    'pwd',
-    'secret',
-    'password_confirmation',
-    'passwordConfirmation',
-    'cc',
-    'card_number',
-    'cardNumber',
-    'ccv',
-    'ssn',
-    'credit_score',
-    'creditScore',
-  ]
-  const fields = [...defaultFieldsToMask, ...additionalFieldsToMask]
-  const fieldsToMask = fields.reduce((acc, field) => {
-    acc[field] = true
-    return acc
-  }, {})
-  return fieldsToMask
-}
-
-function maskSensitiveValues(payload, fieldsToMask) {
-  if (typeof payload !== 'object') return payload
-  if (Array.isArray(payload)) {
-    return payload.map((val) => maskSensitiveValues(val, fieldsToMask))
-  }
-
-  let objectToMask = { ...payload }
-
-  let safeObject = Object.keys(objectToMask).reduce(function (acc, propName) {
-    if (typeof objectToMask[propName] === 'string') {
-      if (fieldsToMask[propName] === true) {
-        acc[propName] = '*'.repeat(objectToMask[propName].length)
-      } else {
-        acc[propName] = objectToMask[propName]
-      }
-    } else if (Array.isArray(objectToMask[propName])) {
-      acc[propName] = objectToMask[propName].map((val) => maskSensitiveValues(val, fieldsToMask))
-    } else if (typeof objectToMask[propName] === 'object') {
-      acc[propName] = maskSensitiveValues(objectToMask[propName], fieldsToMask)
-    } else {
-      acc[propName] = objectToMask[propName]
-    }
-
-    return acc
-  }, {})
-
-  return safeObject
-}
 @inject(['Adonis/Core/Config'])
 export default class Treblle {
   constructor(private config: ConfigContract) {}
@@ -85,7 +25,7 @@ export default class Treblle {
       const trebllePayload = {
         api_key: this.config.get('treblle.apiKey'),
         project_id: this.config.get('treblle.projectId'),
-        version: '0.0.1',
+        version: process.env.npm_package_version,
         sdk: 'adonisjs',
         data: {
           server: {
@@ -123,10 +63,8 @@ export default class Treblle {
         },
         showErrors: this.config.get('treblle.showErrors'),
       }
+      console.log(JSON.stringify(trebllePayload, null, 2))
 
-      // console.log(JSON.stringify(trebllePayload,null,2))
-
-      // @ts-ignore
       fetch('https://rocknrolla.treblle.com', {
         method: 'POST',
         headers: {
